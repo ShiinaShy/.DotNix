1881bbb746dbd1a5ee133f60ffd075b99131ad3c
Shiina <shiina.shy04@gmail.com>
2024-08-17 15:10:16 +0200

    lil comment

--- user/home.nix
+++ user/home.nix
@@ -30,6 +30,8 @@
     home.file.".config/hypr/hyprpaper.conf".source = ../configs/hypr/hyprpaper.conf;
     home.file.".config/hypr/hyprland.conf".source = ../configs/hypr/hyprland.conf;
     home.file.".icons/McMojave" = {
+      # copy of McMojave stored in .DotNix | could've used flake but didn't like it due to unnececary complexity
+      # not happy either but maybe can find better solution in future :3
       source = ../configs/hypr/McMojave;
       recursive = true;
     };
