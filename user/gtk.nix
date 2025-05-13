b6c29aeba61d21eb8e7468b3537723fe9cd9037d
Shiina <shiina.shy04@gmail.com>
2025-05-13 15:50:05 +0200

    changed theme to adw-gtk3 and added temporary qt theme

--- user/gtk.nix
+++ user/gtk.nix
@@ -1,15 +1,30 @@
 # GTK Config
 { config, lib, pkgs, ...}:
 {
-    home-manager.users.shiina.gtk = {
-      enable = true;
-      iconTheme = {
-        name = "Papirus";
-        package = pkgs.papirus-icon-theme;
+  home-manager.users.shiina =
+    let
+        adw-color = "@import '/home/shiina/.cache/wal/colors-gtk.css';";
+    in
+    {
+      gtk = {
+        enable = true;
+        iconTheme = {
+          name = "Papirus";
+          package = pkgs.papirus-icon-theme;
+        };
+        theme = {
+          # name = "Omni";
+          # package = pkgs.omni-gtk-theme;
+          name = "adw-gtk3";
+          package = pkgs.adw-gtk3;
+        };
+        gtk3.extraCss = adw-color;
       };
-      theme = {
-        name = "Omni";
-        package = pkgs.omni-gtk-theme;
+      qt = {
+        enable = true;
+        platformTheme.name = "gtk";
+        style.name = "adwaita-dark";
+        style.package = pkgs.adwaita-qt;
       };
     };
-  }
+}
