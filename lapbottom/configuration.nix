c053f3ab1eae716c8555d4381a75604e93affc8d
Shiina <shiina.shy@gibhug.com>
2025-09-15 12:40:16 +0200

    add networkmanagerapplet for public wifi config

--- lapbottom/configuration.nix
+++ lapbottom/configuration.nix
@@ -87,10 +87,8 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #git
-  #  wget
-  ];
+      networkmanagerapplet # required to connect to some wifi networks
+    ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
