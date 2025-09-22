5786604226957b11687679668678092c106438f3
Shiina <shiina.shy04@gmail.com>
2025-09-22 19:20:58 +0200

    change deskbottom monitor location

--- vars.nix
+++ vars.nix
@@ -2,7 +2,7 @@
   deskbottom = {
     monitor = ''
       monitor=DP-2,3440x1440@144,auto,1,vrr,2
-      monitor=DP-3,1920x1080@60,auto-left,1
+      monitor=DP-3,1920x1080@60,auto-right,1
     '';
     autoStart = "waybar & hyprpaper & discord & keepassxc & kdeconnect-indicator";
     workspaceRules = "workspace = 5, monitor:DP-3, default:true";
