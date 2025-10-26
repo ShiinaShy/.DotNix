019be7d9f60bc12921e164899d0b735ac99eb4a5
Shiina <shiina.shy04@gmail.com>
2025-10-26 12:14:11 +0100

    cleanup and renamed file

--- system/coding.nix
+++ system/coding.nix
@@ -1,11 +1,9 @@
-# Programms needed for Programming in ZIG :3
-{ config, pkgs, ...}:
+# Tools needed for Programming :3
+{ pkgs, ...}:
 {
   environment.systemPackages = with pkgs; [
     zig
     zls
     nixd
-    #zed-editor
-    # (zed-editor.fhsWithPackages (pkgs: [ pkgs.zlib ]))
   ];
 }
