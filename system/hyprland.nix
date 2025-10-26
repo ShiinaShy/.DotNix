7df5aeb848802bc0738f12dbd8935dbadff14832
Shiina <shiina.shy04@gmail.com>
2025-10-26 12:12:17 +0100

    cleanup

--- system/hyprland.nix
+++ system/hyprland.nix
@@ -1,11 +1,11 @@
 # All programms needed to create a "Desktop Enviroment"
-{ config, pkgs, inputs, ...}:
+{ pkgs, ...}:
 {
-  # Enable ddcci kernel Module (had to disable because i have the ONE FUCKING MONITOR THAT BREAKS?! qwq https://github.com/rockowitz/ddcutil/issues/153)
+  # Enable ddcci kernel Module (had to disable because i have the ONE monitor that gets bricked qwq https://github.com/rockowitz/ddcutil/issues/153)
   #boot.extraModulePackages = [config.boot.kernelPackages.ddcci-driver];
   #boot.kernelModules = ["i2c-dev" "ddcci_backlight"];
 
-  # Fix Filemanager Stuff
+  # required for some file manager features like trash 
   services.gvfs.enable = true;
 
   # Enable Audio
@@ -25,7 +25,7 @@
     };
     hyprlock.enable = true;
   };
-  # Electron use Wayland
+  # Make Electron use Wayland
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
   };
