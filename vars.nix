9a738e032a35738a34e5f07fe7945b41fdf174fa
Shiina <shiina.shy04@gmail.com>
2025-12-03 20:09:28 +0100

    new gpu update monitor settings

--- vars.nix
+++ vars.nix
@@ -1,8 +1,8 @@
 {
   deskbottom = {
     monitor = ''
-      monitor=DP-2,3440x1440@144,auto,1,vrr,2
-      monitor=DP-3,1920x1080@60,auto-right,1
+      monitor=DP-1,3440x1440@144,auto-left,1,vrr,1
+      monitor=DP-2,1920x1080@60,auto-right,1
     '';
     autoStart = "waybar & hyprpaper & discord & keepassxc & kdeconnect-indicator";
     workspaceRules = "workspace = 5, monitor:DP-3, default:true";
