2bab8eefa5520604ed0a165456815e282fd34624
Shiina <shiina.shy04@gmail.com>
2026-01-08 12:02:08 +0100

    make line numbers readable

--- user/nvim/colorscheme.lua
+++ user/nvim/colorscheme.lua
@@ -1,11 +1,22 @@
-require("catppuccin").setup({
+require("catppuccin").setup {
     transparent_background = true,
+
     integrations = {
         cmp = true,
         gitsigns = true,
         nvimtree = true,
         treesitter = true,
     },
-})
+
+    -- Set linenumbers to someting readable
+    custom_highlights = function(colors)
+        return {
+            LineNrAbove = { fg = colors.surface3 },
+            LineNr = { fg = "#cba6f7" },
+            LineNrBelow = { fg = colors.surface3 },
+        }
+    end
+}
 
 vim.cmd.colorscheme "catppuccin-mocha"
+
