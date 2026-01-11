681b87da87c99f91bf8c7f6f64e7a8f339a8613b
Shiina <shiina.shy04@gmail.com>
2026-01-11 17:44:10 +0100

    waf 🦊

--- system/hyprland.nix
+++ system/hyprland.nix
@@ -9,6 +9,8 @@
 
   # required for some file manager features like trash 
   services.gvfs.enable = true;
+  
+  environment.etc."/xdg/menus/applications.menu".text = builtins.readFile "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
 
   # Enable Audio
   security.rtkit.enable = true;
