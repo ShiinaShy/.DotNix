86284bbd22e121b7a276010ab52b42ea93231560
Shiina <shiina.shy04@gmail.com>
2026-01-18 10:27:57 +0100

    add credits

--- user/wal/qt-cols.nix
+++ user/wal/qt-cols.nix
@@ -1,3 +1,5 @@
+# This is a modified Version of eylles pywal-libadwaita theme licenced under GPL-3.0
+# You can find it here: https://github.com/eylles/pywal16-libadwaita
 {...}:
 {
   home-manager.users.shiina = {...}: {
