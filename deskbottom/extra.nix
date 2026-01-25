cc56dbee0473a0b6d18761468611be1c61d5eca4
Shiina <shiina.shy04@gmail.com>
2026-01-25 11:30:58 +0100

    switch to flokinet dns

--- deskbottom/extra.nix
+++ deskbottom/extra.nix
@@ -9,5 +9,12 @@
 
   # Enable NTFS Support
   boot.supportedFilesystems = ["ntfs"];
+
+  networking.nameservers = [
+    "185.246.188.51"
+    "37.156.68.20"
+    "2a06:1700:3:11::1"
+    "2a06:1700:100:20::1"
+  ];
 }
 
