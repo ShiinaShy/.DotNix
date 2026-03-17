7e8e5c18d126e10a8e9a6214812a332c1a31adef
Shiina <shiina.shy@gibhug.com>
2026-03-17 19:48:25 +0100

    change names and import required package

--- user/wal/colors.nix
+++ user/wal/colors.nix
@@ -1,26 +1,32 @@
-{ pkgs }:
+{
+  background = "{background}";
+  foreground = "{foreground}";
 
-pkgs.stdenv.mkDerivation {
-  name = "pywal colors";
+  color0 = "{color0}";
+  color1 = "{color1}";
+  color2 = "{color2}";
+  color3 = "{color3}";
+  color4 = "{color4}";
+  color5 = "{color5}";
+  color6 = "{color6}";
+  color7 = "{color7}";
+  color8 = "{color8}";
+  color9 = "{color9}";
+  color10 = "{color10}";
+  color11 = "{color11}";
+  color12 = "{color12}";
+  color13 = "{color13}";
+  color14 = "{color14}";
+  color15 = "{color15}";
 
-  src = builtins.readFile ./pywal-colors.nix;
-  wallpaper = ../wallpaper/fox.png;
-  nativeBuildInputs = [ pkgs.pywal16 ];
-
-  dontUnpack = true;
-
-  preBuild = ''
-    export HOME=$(mktemp -d)
-  '';
-
-  installPhase = ''
-    mkdir $HOME/.config
-    export XDG_CONFIG_HOME=$HOME/.config
-    mkdir $XDG_CONFIG_HOME/wal
-    mkdir $XDG_CONFIG_HOME/wal/templates
-    echo $src > $HOME/.config/wal/templates/cols.nix
-    mkdir garbage
-    wal -i $wallpaper --cols16 --out-dir garbage
-    mv garbage/cols.nix $out
-  '';
+  color0p01 = "{color0.lighten(1%)}";
+  color0p02 = "{color0.lighten(2%)}";
+  color0p03 = "{color0.lighten(3%)}";
+  color0p10 = "{color0.lighten(10%)}";
+  color0p15 = "{color0.lighten(15%)}";
+  color0p70 = "{color0.lighten(70%)}";
+  color8m03 = "{color8.darken(3%)}";
+  backgroundp03 = "{color0.lighten(3%)}";
+  backgroundp70 = "{color0.lighten(70%)}";
 }
+
